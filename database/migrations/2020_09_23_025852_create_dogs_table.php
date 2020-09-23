5528f2cbde8913bf4f858511e1f18f6715a04c4c
moisesduartem <moisesduartem@gmail.com>
2020-09-23 00:01:08 -0300

    adiciona raca e nome

--- database/migrations/2020_09_23_025852_create_dogs_table.php
+++ database/migrations/2020_09_23_025852_create_dogs_table.php
@@ -15,6 +15,8 @@ class CreateDogsTable extends Migration
     {
         Schema::create('dogs', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('raca');
+            $table->string('nome');
             $table->timestamps();
         });
     }
